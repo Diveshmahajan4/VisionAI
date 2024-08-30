074bc25cbab63fff6ad372b45571c81c696d84c1
Diveshmahajan4 <diveshmahajan04@gmail.com>
2024-08-30 16:18:20 +0530

    formatting scripts file

--- scripts/sharegpt4v/eval/seed.sh
+++ scripts/sharegpt4v/eval/seed.sh
@@ -42,4 +42,3 @@ python scripts/convert_seed_for_submission.py \
     --annotation-file ./playground/data/eval/seed_bench/SEED-Bench.json \
     --result-file $output_file \
     --result-upload-file ./playground/data/eval/seed_bench/answers_upload/${CKPT_NAME}.jsonl
-
